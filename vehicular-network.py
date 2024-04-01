c155357cd7b7d8904d165979449a9c89c849a2c3
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2024-04-01 14:04:17 +0300

    Update vehicular-network.py

--- vehicular-network.py
+++ vehicular-network.py
@@ -1,10 +1,11 @@
 from mininet.net import Mininet
-from mininet.node import Controller, OVSKernelAP, Car
+from mininet.node import Controller, Car
+from mininet.wifi.node import AccessPoint
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
 def vehicularNetwork():
-    net = Mininet(controller=Controller, accessPoint=OVSKernelAP)
+    net = Mininet(controller=Controller)
 
     print("*** Creating nodes")
     # Adding vehicles
@@ -17,21 +18,23 @@ def vehicularNetwork():
     sta1 = net.addStation('sta1', position='10,20,0')
     sta2 = net.addStation('sta2', position='30,40,0')
 
+    # Adding access points
+    ap1 = net.addAccessPoint('ap1', ssid='V2I', mode='g', channel='1', position='20,30,0')
+    ap2 = net.addAccessPoint('ap2', ssid='meshNetwork', mode='g', channel='6', position='40,50,0')
+
     print("*** Creating links")
-    # Creating links between vehicles and stations
-    net.addMesh(v1, intf='v1-wlan0', ssid='meshNetwork')
-    net.addMesh(v2, intf='v2-wlan0', ssid='meshNetwork')
-    net.addMesh(v3, intf='v3-wlan0', ssid='meshNetwork')
-    net.addMesh(v4, intf='v4-wlan0', ssid='meshNetwork')
+    # Creating links between vehicles and access points
+    net.addLink(v1, ap1)
+    net.addLink(v2, ap1)
+    net.addLink(v3, ap1)
+    net.addLink(v4, ap1)
+
+    # Creating links between stations and access points
+    net.addLink(sta1, ap2)
+    net.addLink(sta2, ap2)
 
     # Enabling V2I communication using 802.11p protocol
-    net.addL2Mesh('v1', intf='v1-wlan0', ssid='V2I', mode='80211p')
-    net.addL2Mesh('v2', intf='v2-wlan0', ssid='V2I', mode='80211p')
-    net.addL2Mesh('v3', intf='v3-wlan0', ssid='V2I', mode='80211p')
-    net.addL2Mesh('v4', intf='v4-wlan0', ssid='V2I', mode='80211p')
-
-    # Creating link between stations
-    net.addLink(sta1, sta2)
+    ap1.setMeshIface('ap1-wlan1', ssid='V2I', mode='80211p')
 
     print("*** Starting network")
     net.build()
