424e14e8d08a20ddadac700cd2b667e8875daf92
pnasis <prodromos.nasis@gmail.com>
2024-04-04 11:55:07 -0400

    code

--- example.py
+++ example.py
@@ -25,39 +25,50 @@ def topology():
     net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
 
     info("*** Creating nodes\n")
-    for id in range(0, 10):
+    for id in range(0, 4):
         net.addCar('car%s' % (id+1), wlans=2, encrypt=['wpa2', ''])
 
-    kwargs = {'ssid': 'vanet-ssid', 'mode': 'g', 'passwd': '123456789a',
-              'encrypt': 'wpa2', 'failMode': 'standalone', 'datapath': 'user'}
-    e1 = net.addAccessPoint('e1', mac='00:00:00:11:00:01', channel='1',
-                            position='2600,3500,0', **kwargs)
-    e2 = net.addAccessPoint('e2', mac='00:00:00:11:00:02', channel='6',
-                            position='2800,3500,0', **kwargs)
-    e3 = net.addAccessPoint('e3', mac='00:00:00:11:00:03', channel='11',
-                            position='3000,3500,0', **kwargs)
-    e4 = net.addAccessPoint('e4', mac='00:00:00:11:00:04', channel='1',
-                            position='2600,3300,0', **kwargs)
-    e5 = net.addAccessPoint('e5', mac='00:00:00:11:00:05', channel='6',
-                            position='2800,3300,0', **kwargs)
-    e6 = net.addAccessPoint('e6', mac='00:00:00:11:00:06', channel='11',
-                            position='3000,3300,0', **kwargs)
+    sta1 = net.addStation('sta1', ip='10.0.0.1/8', position='100,150,0')
+    sta2 = net.addStation('sta2', ip='10.0.0.2/8', position='150,150,0')
+    sta3 = net.addStation('sta3', ip='10.0.0.3/8', position='200,150,0')
+    #kwargs = {'ssid': 'vanet-ssid', 'mode': 'g', 'passwd': '123456789a',
+              #'encrypt': 'wpa2', 'failMode': 'standalone', 'datapath': 'user'}
+    #e1 = net.addAccessPoint('e1', mac='00:00:00:11:00:01', channel='1',
+                            #position='2600,3500,0', **kwargs)
+    #e2 = net.addAccessPoint('e2', mac='00:00:00:11:00:02', channel='6',
+                            #position='2800,3500,0', **kwargs)
+    #e3 = net.addAccessPoint('e3', mac='00:00:00:11:00:03', channel='11',
+                            #position='3000,3500,0', **kwargs)
+    #e4 = net.addAccessPoint('e4', mac='00:00:00:11:00:04', channel='1',
+                            #position='2600,3300,0', **kwargs)
+    #e5 = net.addAccessPoint('e5', mac='00:00:00:11:00:05', channel='6',
+                            #position='2800,3300,0', **kwargs)
+    #e6 = net.addAccessPoint('e6', mac='00:00:00:11:00:06', channel='11',
+                            #position='3000,3300,0', **kwargs)
 
     info("*** Configuring Propagation Model\n")
-    net.setPropagationModel(model="logDistance", exp=2.8)
+    net.setPropagationModel(model="logDistance", exp=3.5)
 
     info("*** Configuring nodes\n")
     net.configureNodes()
 
-    net.addLink(e1, e2)
-    net.addLink(e2, e3)
-    net.addLink(e3, e4)
-    net.addLink(e4, e5)
-    net.addLink(e5, e6)
+    #net.addLink(e1, e2)
+    #net.addLink(e2, e3)
+    #net.addLink(e3, e4)
+    #net.addLink(e4, e5)
+    #net.addLink(e5, e6)
+    net.addLink(sta1,sta2)
+    net.addLink(sta2,sta3)
     for car in net.cars:
         net.addLink(car, intf=car.wintfs[1].name,
                     cls=ITSLink, band=20, channel=181)
 
+    net.addLink(sta1, intf='sta1-wlan0', cls=ITSLink,
+                band=20, channel=181, proto='batman_adv')
+    net.addLink(sta2, intf='sta2-wlan0', cls=ITSLink,
+                band=20, channel=181, proto='batman_adv')
+    net.addLink(sta3, intf='sta3-wlan0', cls=ITSLink,
+                band=20, channel=181, proto='batman_adv')
     # exec_order: Tells TraCI to give the current
     # client the given position in the execution order.
     # We may have to change it from 0 to 1 if we want to
@@ -67,11 +78,17 @@ def topology():
                            extra_params=["--start --delay 1000"],
                            clients=1, exec_order=0)
 
+    #info("*** Plotting Graph\n")
+    #net.plotGraph(max_x=300, max_y=300)
+
     info("*** Starting network\n")
     net.build()
 
-    for enb in net.aps:
-        enb.start([])
+    #for enb in net.aps:
+        #enb.start([])
+
+    #for enb in net.stations:
+	#enb.start([])
 
     for id, car in enumerate(net.cars):
         car.setIP('192.168.0.{}/24'.format(id+1),
@@ -80,7 +97,7 @@ def topology():
                   intf='{}'.format(car.wintfs[1].name))
 
     # Track the position of the nodes
-    nodes = net.cars + net.aps
+    nodes = net.cars + net.stations
     net.telemetry(nodes=nodes, data_type='position',
                   min_x=2200, min_y=2800,
                   max_x=3200, max_y=3900)
--- experiment.py
+++ experiment.py
@@ -13,18 +13,21 @@ def topology():
 	net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
 
 	info("*** Creating nodes\n")
-	sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', ip='10.0.0.1/8', position='200,400,0')
-	sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', ip='10.0.0.2/8', position='400,400,0')
-	car1 = net.addCar('car1', wlans=2, mac='00:00:00:00:00:03', ip='10.0.0.3/8', position='150,200,0')
-	car1 = net.addCar('car2', wlans=2, mac='00:00:00:00:00:04', ip='10.0.0.4/8', position='250,200,0')
-	car1 = net.addCar('car3', wlans=2, mac='00:00:00:00:00:05', ip='10.0.0.5/8', position='350,200,0')
-	car1 = net.addCar('car4', wlans=2, mac='00:00:00:00:00:06', ip='10.0.0.6/8', position='450,200,0')
-	c1 = net.addController('c1')
+	kwargs = {'ssid': 'vanet-ssid', 'mode': 'g', 'passwd': '123456789a',
+              'encrypt': 'wpa2', 'failMode': 'standalone', 'datapath': 'user'}
+    	ap1 = net.addAccessPoint('ap1', mac='00:00:00:11:00:01', channel='1',
+                            position='2600,3500,0', **kwargs)
+	sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', position='200,400,0')
+	sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', position='400,400,0')
+	car1 = net.addCar('car1', wlans=2, mac='00:00:00:00:00:03', encrypt=['wpa2', ''], position='150,200,0')
+	car1 = net.addCar('car2', wlans=2, mac='00:00:00:00:00:04', encrypt=['wpa2', ''], position='250,200,0')
+	car1 = net.addCar('car3', wlans=2, mac='00:00:00:00:00:05', encrypt=['wpa2', ''], position='350,200,0')
+	car1 = net.addCar('car4', wlans=2, mac='00:00:00:00:00:06', encrypt=['wpa2', ''], position='450,200,0')
+#	c1 = net.addController('c1')
 
 	#bs1 = net.addBaseStation('BS1', ssid='new-ssid1', mode='g', channel='1')
 	#bs2 = net.addBaseStation('BS2', ssid='new-ssid2', mode='g', channel='6')
 	#bs3 = net.addBaseStation('BS3', ssid='new-ssid3', mode='g', channel='11')
-	#c1 = new.addController('c1')
 
 	info("*** Configuring Propagation Model\n")
 	net.setPropagationModel(model="logDistance", exp=3.5)
@@ -41,7 +44,7 @@ def topology():
         	        band=20, channel=181, proto='batman_adv')
     	net.addLink(sta2, intf='sta2-wlan0', cls=ITSLink,
         	        band=20, channel=181, proto='batman_adv')
-	
+
 	#for x in range(0,20):
 	#	net.addMesh(car[x], ssid='mesh')
 
@@ -52,7 +55,14 @@ def topology():
 
 	info("*** Starting network\n")
 	net.build()
-	c1.start()
+#	c1.start()
+	ap1.start([])
+
+	for id, car in enumerate(net.cars):
+        	car.setIP('192.168.0.{}/24'.format(id+1),
+                	  intf='{}'.format(car.wintfs[0].name))
+        	car.setIP('192.168.1.{}/24'.format(id+1),
+                	  intf='{}'.format(car.wintfs[1].name))
 #	sta1.start([c1])
 #	sta2.start([c1])
 #	sta3.start([c1])
