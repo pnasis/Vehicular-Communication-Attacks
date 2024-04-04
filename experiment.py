4e202bd71e50fa6e79d276a3f27a26ddb08e29bd
pnasis <prodromos.nasis@gmail.com>
2024-04-04 07:08:01 -0400

    first

--- experiment.py
+++ experiment.py
@@ -13,13 +13,13 @@ def topology():
 	net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
 
 	info("*** Creating nodes\n")
-	sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', ip='10.0.0.1/8', position='100,150,0')
-    sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', ip='10.0.0.2/8', position='150,150,0')
-	car1 = net.addCar('car1', wlans=2, mac='00:00:00:00:00:03', ip='10.0.0.3/8', position='20,100,0')
-	car1 = net.addCar('car2', wlans=2, mac='00:00:00:00:00:04', ip='10.0.0.4/8', position='40,100,0')
-	car1 = net.addCar('car3', wlans=2, mac='00:00:00:00:00:05', ip='10.0.0.5/8', position='60,100,0')
-	car1 = net.addCar('car4', wlans=2, mac='00:00:00:00:00:06', ip='10.0.0.6/8', position='80,100,0')
-	c1 = new.addController('c1')
+	sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', ip='10.0.0.1/8', position='200,400,0')
+	sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', ip='10.0.0.2/8', position='400,400,0')
+	car1 = net.addCar('car1', wlans=2, mac='00:00:00:00:00:03', ip='10.0.0.3/8', position='150,200,0')
+	car1 = net.addCar('car2', wlans=2, mac='00:00:00:00:00:04', ip='10.0.0.4/8', position='250,200,0')
+	car1 = net.addCar('car3', wlans=2, mac='00:00:00:00:00:05', ip='10.0.0.5/8', position='350,200,0')
+	car1 = net.addCar('car4', wlans=2, mac='00:00:00:00:00:06', ip='10.0.0.6/8', position='450,200,0')
+	c1 = net.addController('c1')
 
 	#bs1 = net.addBaseStation('BS1', ssid='new-ssid1', mode='g', channel='1')
 	#bs2 = net.addBaseStation('BS2', ssid='new-ssid2', mode='g', channel='6')
@@ -27,38 +27,35 @@ def topology():
 	#c1 = new.addController('c1')
 
 	info("*** Configuring Propagation Model\n")
-    net.setPropagationModel(model="logDistance", exp=3.5)
+	net.setPropagationModel(model="logDistance", exp=3.5)
 
-    info("*** Configuring nodes\n")
-    net.configureNodes()
-    for car in net.cars:
-        net.addLink(car, intf=car.wintfs[1].name,
-                    cls=ITSLink, band=20, channel=181)
+    	info("*** Configuring nodes\n")
+    	net.configureNodes()
+    	for car in net.cars:
+        	net.addLink(car, intf=car.wintfs[1].name,
+                    	cls=ITSLink, band=20, channel=181)
 
 
 	info("*** Starting ITS Links\n")
-    net.addLink(sta1, intf='sta1-wlan0', cls=ITSLink,
-                band=20, channel=181, proto='batman_adv')
-    net.addLink(sta2, intf='sta2-wlan0', cls=ITSLink,
-                band=20, channel=181, proto='batman_adv')
-    net.addLink(sta3, intf='sta3-wlan0', cls=ITSLink,
-                band=20, channel=181, proto='batman_adv')
+    	net.addLink(sta1, intf='sta1-wlan0', cls=ITSLink,
+        	        band=20, channel=181, proto='batman_adv')
+    	net.addLink(sta2, intf='sta2-wlan0', cls=ITSLink,
+        	        band=20, channel=181, proto='batman_adv')
 	
-	for x in range(0,20):
-		net.addMesh(car[x], ssid='mesh')
+	#for x in range(0,20):
+	#	net.addMesh(car[x], ssid='mesh')
 
-	net.addLink(bs1, bs2)
-	net.addLink(bs1, bs3)
+#	net.addLink(bs1, bs2)
+#	net.addLink(bs1, bs3)
 
-	if '-p' not in args:
-	    net.plotGraph(max_x=600, max_y=600)
+	net.plotGraph(max_x=600, max_y=600)
 
 	info("*** Starting network\n")
 	net.build()
 	c1.start()
-	sta1.start([c1])
-	sta2.start([c1])
-	sta3.start([c1])
+#	sta1.start([c1])
+#	sta2.start([c1])
+#	sta3.start([c1])
 
 	info("*** Running CLI\n")
 	CLI(net)
